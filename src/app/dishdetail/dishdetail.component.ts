c9d2ece1869bf0289fb39aaf5b1dd74d8914cb5d
Marianela <marianela_min@yahoo.com>
2019-05-23 13:20:45 -0400

    simgle page application part 2: added passing of information through angular routes and making use of the services we already had created.

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
+
 import { Dish } from '../shared/dish';
 
 @Component({
@@ -8,12 +13,18 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
+  // @Input() no longer need it, since it wont be received as input
   dish: Dish;
 
-  constructor() { }
+  constructor( private dishService: DishService, private location: Location,
+    private route: ActivatedRoute) { }
   
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void{
+    this.location.back();
+  }
 }
