7e4c4f3e24cd52adce204cf56c4a50785ef4e1e7
Marianela <marianela_min@yahoo.com>
2019-05-23 10:54:50 -0400

    I might have to delete this.. for now I will comment the content of the whole document

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// // when we add a new module using the cli
+// // this gets added import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   //there is no exports
+//   exports: [RouterModule]
+//   // this gets added aswell, declarations: []
+// })
+// export class AppRoutingModule { }
