b0606f4800bcb260d8c0de008248dfe1ae45e062
Marianela <marianela_min@yahoo.com>
2019-05-23 12:07:24 -0400

    Simgle page application part 1 - added promotion class and service, added content to homepage and injecting promotion and dish services

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,7 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path:'menu', component: MenuComponent},
+  {path:'contactus', component: ContactComponent},
   {path:'aboutus', component: AboutComponent},
-  {path:'contact', component: ContactComponent},
   {path:'', redirectTo: 'home', pathMatch: 'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
-
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -49,7 +49,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule
   ],
   providers: [ 
-    DishService
+    DishService,
+    PromotionService
    ],
   bootstrap: [AppComponent]
 })
