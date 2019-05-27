361d7cfa57147ad0e4c0b42017cb990ebd3a0404
Marianela <marianela_min@yahoo.com>
2019-05-27 10:37:30 -0400

    added a dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import{ MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
 
@@ -20,6 +20,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
@@ -37,7 +38,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -48,12 +50,17 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    // MatDialogRef
   ],
   providers: [ 
     DishService,
     PromotionService,
     LeaderService
+   ],
+  entryComponents:[
+    LoginComponent
    ],
   bootstrap: [AppComponent]
 })
