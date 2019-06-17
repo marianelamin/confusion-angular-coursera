6dbaa8899200a6960592d2a3815ee9714b4d0809
Marianela <marianela_min@yahoo.com>
2019-06-17 12:41:38 -0400

    RxJs part 2.  Exploring more on observables using the route.params observable

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,6 +6,8 @@ import { DishService } from '../services/dish.service';
 
 import { Dish } from '../shared/dish';
 
+import { switchMap } from 'rxjs/operators'
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -15,14 +17,27 @@ export class DishdetailComponent implements OnInit {
 
   // @Input() no longer need it, since it wont be received as input
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor( private dishService: DishService, private location: Location,
     private route: ActivatedRoute) { }
   
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-    .subscribe((dish) => this.dish= dish);
+    this.dishService.getDishIds()
+    .subscribe((dishIds) => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
+    .subscribe((dish) => {
+      this.dish = dish;
+      this.setPrevNext(dish.id);
+    });
+  }
+
+  setPrevNext(dishId: string){
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
   }
 
   goBack(): void{
