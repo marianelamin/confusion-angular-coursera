893bdac1533f0e025f8a97164a1e0b2805c982e8
Marianela <marianela_min@yahoo.com>
2019-06-19 15:42:45 -0400

    custome attribute directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
