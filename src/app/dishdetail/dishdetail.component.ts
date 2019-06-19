d11de9fc2cbd1bed8abca4ea13b57c47341154d6
Marianela <marianela_min@yahoo.com>
2019-06-19 16:28:56 -0400

    animations part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,12 +8,27 @@ import { switchMap } from 'rxjs/operators'
 
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations:[
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity : 1
+      })),
+      state('hidden', style(
+        {
+          transform: 'scale(0.5)',
+          opacity : 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -29,6 +44,8 @@ export class DishdetailComponent implements OnInit {
   @ViewChild('cform') commentFormDirective;
 
   dishcopy: Dish;
+  visibility ='shown';
+
 
   formErrors = {
     'comment' : 'nancyC',
@@ -54,14 +71,14 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishService.getDishIds()
     .subscribe((dishIds) => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
-    .subscribe(
-      (dish) => {
-                  this.dish = dish;
-                  this.dishcopy = dish;
-                  this.setPrevNext(dish.id);
-                },
-      (errmess) => this.errMess = <any>errmess );
+    this.route.params.pipe(switchMap((params:Params) => { this.visibility = 'hidden'; return this.dishService.getDish(params['id']); }))
+    .subscribe(      (dish) => {
+                                  this.visibility = 'shown';
+                                  this.dish = dish;
+                                  this.dishcopy = dish;
+                                  this.setPrevNext(dish.id);
+                                },
+                      (errmess) => this.errMess = <any>errmess );
   }
 
   // **************************** ALL ABOUT PREV AND NEXT ITEM ****************************
